6e38135db58f0ef5a37f22eec36db7ab5a72d340
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2018-04-18 17:16:57 +0200

    Fully featured API

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-const url = process.env.MONGO_URL;
-const dbName = process.env.DB_NAME;
+app.use(bodyParser.json())
+
+const url = process.env.MONGO_URL,
+    dbName = process.env.DB_NAME,
+    port = process.env.PORT;
 
 const MongoClient = require('mongodb').MongoClient;
 
@@ -13,22 +20,59 @@ let exampleScore = {
     min: 1
 }
 
-MongoClient.connect(url, function (err, client) {
-    console.assert(err == null);
-
+MongoClient.connect(url).then(client => {
     const db = client.db(dbName);
     const scoreboard = db.collection('scoreboard')
 
-    app.get('/scoreboard', (req, res) => {
-        scoreboard.find()
+    app.get('/scores', (req, res) => {
+        scoreboard
+            .find()
+            .project({ name: 1, time: 1, max: 1, min: 1, _id: 0 })
             .limit(10)
             .sort({ time: 1 })
             .toArray()
             .then(arr => {
-                console.log(arr)
+                res.json(arr)
             })
     })
-    app.post('/score', (req, res) => {
-        console.log(req.body)
+    app.post('/scores', (req, res) => {
+        let b = req.body;
+        let doc = {
+            time: b.time,
+            name: b.name,
+            max: b.max,
+            min: b.min,
+            ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        }
+        if (!doc.time) {
+            res.status(400).json({
+                error: "Time is required"
+            })
+            return
+        }
+        if (doc.time < 0) {
+            res.status(400).json({
+                error: "Time is less than zero! Hmm..."
+            })
+            return
+        }
+        if (doc.max < doc.min) {
+            res.status(400).json({
+                error: "Max must not be less than min."
+            })
+            return
+        }
+        if (doc.min > 0) {
+            res.status(400).json({
+                error: "Minimum must be more than zero."
+            })
+            return
+        }
+        scoreboard.insertOne(doc).then(() => {
+            res.sendStatus(200);
+        })
     })
-});
+    app.listen(port);
+}).catch(err => {
+    console.error(err);
+})
